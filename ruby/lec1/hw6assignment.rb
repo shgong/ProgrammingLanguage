806e82d868edce8552cc0dab0798b5bae4362ee1
Booker <bgong001c@cable.comcast.com>
2017-05-11 16:34:16 -0400

    reuse MyPiece

--- ruby/lec1/hw6assignment.rb
+++ ruby/lec1/hw6assignment.rb
@@ -5,30 +5,21 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-               rotations([[0, 0], [-1,0], [1, 0], [0, -1], [-1, -1]]), # sigma
-               [[[0, 0], [-1, 0], [1, 0], [2, 0], [3,0]], # long five (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2], [0,3]]],
-               rotations([[0, 0], [-1, 0], [0, -1]])] # small L
+  All_My_Pieces = All_Pieces +
+    [rotations([[0, 0], [-1,0], [1, 0], [0, -1], [-1, -1]]), # sigma
+     [[[0, 0], [-1, 0], [1, 0], [2, 0], [3,0]], # long five (only needs two)
+      [[0, 0], [0, -1], [0, 1], [0, 2], [0,3]]],
+     rotations([[0, 0], [-1, 0], [0, -1]])] # small L
 
-
+  Cheat_Piece = [[0,0]]
 
   # your enhancements here
-
-  # class method to choose the next piece
   def self.next_piece (board)
     Piece.new(All_My_Pieces.sample, board)
   end
 
   def self.cheat_piece(board)
-    Piece.new([[0,0]], board)
+    Piece.new(Cheat_Piece, board)
   end
 
 end
@@ -37,11 +28,8 @@ class MyBoard < Board
   # your enhancements here
 
   def initialize (game)
-    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    super(game)
     @current_block = MyPiece.next_piece(self)
-    @score = 0
-    @game = game
-    @delay = 500
     @cheating = false
   end
 
