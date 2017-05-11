06961b6237254ad299565ef3ead8a9241683f8bd
Booker <bgong001c@cable.comcast.com>
2017-05-11 14:34:33 -0400

    u key and new blocks

--- ruby/lec1/hw6assignment.rb
+++ ruby/lec1/hw6assignment.rb
@@ -5,19 +5,81 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+               rotations([[0, 0], [-1,0], [1, 0], [0, -1], [-1, -1]]), # sigma
+               [[[0, 0], [-1, 0], [1, 0], [2, 0], [3,0]], # long five (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2], [0,3]]],
+               rotations([[0, 0], [-1, 0], [0, -1]])] # small L
+
+
 
   # your enhancements here
+
+  # class method to choose the next piece
+  def self.next_piece (board)
+    Piece.new(All_My_Pieces.sample, board)
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
 
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..(locations.size-1)).each{|index|
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] =
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
+  def rotate_180_degree
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
+  end
+
+
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+    super
+    @root.bind('u', proc {@board.rotate_180_degree})
+  end
 
 end
-
-
